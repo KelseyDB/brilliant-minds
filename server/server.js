4754366330771d9710cf62ea7bcf751d18981677
Kelsey De Busschere <kelseydebusschere@gmail.com>
2023-12-22 16:07:53 +0100

    Add port to pool

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const pool = mariadb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   conectionLimit: 5
 
 });
